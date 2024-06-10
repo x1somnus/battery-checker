e76baf85bed33b9732bd658140056ec212722aca
x1somnus <beafjunker@gmail.com>
2024-06-11 00:16:56 +0900

    バッテリー番号の違いに影響を受けない汎用化＆取得情報の取捨選択を行った

--- bat-check.sh
+++ bat-check.sh
@@ -1,1 +1,1 @@
-upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep -e "percentage" -e "time to empty"
+upower -e | grep -e "battery" | sed 's/^/upower -i /' | bash | grep -e "updated" -e "energy" -e "energy-full" -e "energy-full-design" -e "time to empty" -e "percentage" -e "capacity"
